a7a1fd278db42d0ae125904f57269b9af60498e3
Oscar Tahual <borussiadortmundyes@gmail.com>
2026-02-26 20:14:08 -0600

    comit 2 antes del switch

--- git 1/Program.cs
+++ git 1/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 List<double> nota = new List<double>();
+bool continuar = true;
+int opcion;
+do
+{
+    Console.Clear();
+    Console.WriteLine("1)ingresar notas");
+    Console.WriteLine("2) ver nota");
+    Console.WriteLine("3) salir");
+    opcion = int.Parse(Console.ReadLine());
+} while (continuar = false);
