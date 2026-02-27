86bacb01290e97c51a179c98913addb61281a78c
Oscar Tahual <borussiadortmundyes@gmail.com>
2026-02-26 20:16:22 -0600

    comit final

--- git 1/Program.cs
+++ git 1/Program.cs
@@ -10,4 +10,27 @@ do
     Console.WriteLine("2) ver nota");
     Console.WriteLine("3) salir");
     opcion = int.Parse(Console.ReadLine());
-} while (continuar = false);
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("ingrese la nota");
+            double n = double.Parse(Console.ReadLine());
+            nota.Add(n);
+            break;
+        case 2:
+            Console.WriteLine("las notas ingresadas son:");
+            foreach (double item in nota)
+            {
+                Console.WriteLine(item);
+            }
+            break;
+        case 3:
+            continuar = false;
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            break;
+        }
+
+    } while (continuar = false);
+
